3aee2b34cb9c2c0865e2e4367b7997b2965692e1
Gabe Abreu de Araujo <g.abreu958@gmail.com>
2023-06-14 05:58:33 -0300

    pagina tempo

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,14 +1,29 @@
 import { execOnce } from "next/dist/shared/lib/utils";
 
-function Tempo(){
+function Tempo(props){
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
 
     return(
         <div>
+            <div>
             {dynamicDateString} (dinâmico)
+            </div>
+            <div>
+            {props.staticDateString} (estático)
+            </div>
         </div>
     )
 }
 
+export function getStaticProps(){
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return{
+        props:{
+            staticDateString
+        }
+    }
+}
 export default Tempo;
